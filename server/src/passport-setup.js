a34f6bd205aa515e364d0d6753b8937c86bd76c2
Philippe Zénone <philippe.zenone@free.fr>
2025-07-09 09:05:40 +0200

    Google OAuth 2

--- server/src/passport-setup.js
+++ server/src/passport-setup.js
@@ -23,7 +23,9 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "/auth/google/callback",
+            callbackURL: process.env.NODE_ENV === 'production' 
+                ? "https://server.be-out-app.dedibox2.philippezenone.net/auth/google/callback"
+                : "http://localhost:3000/auth/google/callback",
             scope: ["profile", "email"],
         },
         async (accessToken, refreshToken, profile, done) => {
