87c38d3c01f3a347fac57d2c3bdd658a5a9c6844
P-ZeN <philippe.zenone@free.fr>
2025-08-06 09:34:35 +0200

    IOS Google OAuth 2 debug 21

--- tauri-plugin-google-auth/ios/Package.swift
+++ tauri-plugin-google-auth/ios/Package.swift
@@ -1,29 +1,34 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
     name: "tauri-plugin-google-auth",
     platforms: [
-        .iOS(.v15),
-        .macOS(.v10_15)
+        .macOS(.v10_13),
+        .iOS(.v13),
     ],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "tauri-plugin-google-auth",
             type: .static,
-            targets: ["tauri-plugin-google-auth"]
-        )
+            targets: ["tauri-plugin-google-auth"]),
     ],
     dependencies: [
+        .package(name: "Tauri", path: "../.tauri/tauri-api"),
         .package(name: "GoogleSignIn", url: "https://github.com/google/GoogleSignIn-iOS", from: "6.2.0")
     ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "tauri-plugin-google-auth",
             dependencies: [
+                .product(name: "Tauri", package: "Tauri"),
                 .product(name: "GoogleSignIn", package: "GoogleSignIn")
             ],
-            path: "Sources"
-        )
+            path: "Sources"),
     ]
 )
