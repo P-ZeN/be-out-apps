3d03d8dabd00d45c4f1fb9a3298e45748bea8787
P-ZeN <philippe.zenone@free.fr>
2025-10-01 16:18:34 +0200

    fix: correct showroom Docker configuration to match working client setup
    - Revert nginx.conf location to /etc/nginx/nginx.conf (complete config file)
    - Update nginx.conf to include http block and worker configuration like client
    - Modify docker-entrypoint.sh to start nginx daemon instead of exec command
    - Matches the working client Docker pattern exactly

--- showroom/docker-entrypoint.sh
+++ showroom/docker-entrypoint.sh
@@ -8,5 +8,5 @@ if [ ! -z "$VITE_API_URL" ]; then
     find /usr/share/nginx/html -name "*.js" -exec sed -i "s|PLACEHOLDER_API_URL|$VITE_API_URL|g" {} \;
 fi
 
-# Execute the original command
-exec "$@"
+# Start nginx
+exec nginx -g 'daemon off;'
